2b8554db1d1202eea7492b92c2dd8d6a1021e6d1
tingting tao <tingting.tao@tu-braunschweig.de>
2025-10-11 23:38:54 +0200

    feat: gateway api modify

--- gateway/internal/handler/api/loginhandler.go
+++ gateway/internal/handler/api/loginhandler.go
@@ -1,27 +1,27 @@
 // Code scaffolded by goctl. Safe to edit.
 // goctl 1.9.1
 
-package handler
+package api
 
 import (
 	"net/http"
 
-	"github.com/uwu-octane/antBackend/gateway/internal/logic"
+	"github.com/uwu-octane/antBackend/gateway/internal/logic/api"
 	"github.com/uwu-octane/antBackend/gateway/internal/svc"
 	"github.com/uwu-octane/antBackend/gateway/internal/types"
 	"github.com/zeromicro/go-zero/rest/httpx"
 )
 
-func LoginRequestHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+func LoginHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var req types.Request
+		var req types.LoginReq
 		if err := httpx.Parse(r, &req); err != nil {
 			httpx.ErrorCtx(r.Context(), w, err)
 			return
 		}
 
-		l := logic.NewLoginRequestLogic(r.Context(), svcCtx)
-		resp, err := l.LoginRequest(&req)
+		l := api.NewLoginLogic(r.Context(), svcCtx)
+		resp, err := l.Login(&req)
 		if err != nil {
 			httpx.ErrorCtx(r.Context(), w, err)
 		} else {
